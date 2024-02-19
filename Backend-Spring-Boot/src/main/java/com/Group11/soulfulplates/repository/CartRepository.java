0291211c8bdc7e96b8731b7974e940ea3c46a58b
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-02-20 00:28:03 +0530

    feat(Backend): Cart Repository added to get carts

---
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/repository/CartRepository.java
@@ -0,0 +1,40 @@
+package com.Group11.soulfulplates.repository;
+
+import com.Group11.soulfulplates.models.Cart;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class CartRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    // Find a cart by user ID and serviceProviderId
+    public Optional<Cart> findByUserIdAndServiceProviderId(Long userId, Long serviceProviderId) {
+        Cart cart = entityManager.createQuery("SELECT c FROM Cart c WHERE c.userId = :userId AND c.serviceProviderId = :serviceProviderId", Cart.class)
+                .setParameter("userId", userId)
+                .setParameter("serviceProviderId", serviceProviderId)
+                .getResultList()
+                .stream()
+                .findFirst()
+                .orElse(null);
+        return Optional.ofNullable(cart);
+    }
+
+    // Method to find a cart by user ID
+    public Optional<Cart> findByUserId(Long userId) {
+        try {
+            Cart cart = entityManager.createQuery("SELECT c FROM Cart c WHERE c.userId = :userId", Cart.class)
+                    .setParameter("userId", userId)
+                    .getSingleResult();
+            return Optional.of(cart);
+        } catch (Exception e) {
+            // Handle no result or any other exceptions appropriately
+            return Optional.empty();
+        }
+    }
+}
