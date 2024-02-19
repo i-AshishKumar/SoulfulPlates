a56131f4259543a8228f757b929376ec877e65cd
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-02-20 00:18:31 +0530

    feat(Backend): Service Impl to create and update carts by user and seller ids
    CartServiceImplementation added to create and update carts by user and seller ids

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/CartServiceImpl.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/CartServiceImpl.java
@@ -24,4 +24,35 @@ public class CartServiceImpl implements CartService {
         return cartRepository.findByUserId(userId);
     }
 
+//    @Override
+//    public Cart createOrUpdateCart(Long userId) {
+//        Cart cart = cartRepository.findByUserId(userId)
+//                .orElse(new Cart());
+//
+//        if (cart.getCartId() == null) {
+//            cart.setUserId(userId);
+//            cart.setCreatedDate(LocalDateTime.now());
+//        }
+//        cart.setLastUpdatedDate(LocalDateTime.now()); // Update the last updated date
+//        return cartRepository.save(cart); // Save the cart and return it
+//    }
+
+    @Override
+    public Cart createOrUpdateCart(Long userId, Long sellerId) {
+        // Check if a cart exists for the given userId and sellerId
+        Optional<Cart> existingCart = cartRepository.findByUserIdAndServiceProviderId(userId, sellerId);
+        if (existingCart.isPresent()) {
+            // If cart exists, return the existing one
+            return existingCart.get();
+        } else {
+            // If not, create a new cart
+            Cart newCart = new Cart();
+            newCart.setUserId(userId);
+            newCart.setServiceProviderId(sellerId);
+            newCart.setCreatedDate(LocalDateTime.now());
+            newCart.setLastUpdatedDate(LocalDateTime.now());
+            return cartRepository.save(newCart);
+        }
+    }
+
 }
