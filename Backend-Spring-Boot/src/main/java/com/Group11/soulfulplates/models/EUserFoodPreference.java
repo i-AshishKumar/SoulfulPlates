24c0da853679e2e4cfa25d99135f49b51917be47
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-02-19 21:28:14 -0400

    feat(Backend): Add UserFoodPreference entity model and UserFoodPreference Enums

---
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/models/EUserFoodPreference.java
@@ -0,0 +1,10 @@
+package com.Group11.soulfulplates.models;
+
+public enum EUserFoodPreference {
+        VEGETARIAN,
+        VEGAN,
+        GLUTEN_FREE,
+        NO_ONIONS,
+        // Add more preference types as needed
+}
+
