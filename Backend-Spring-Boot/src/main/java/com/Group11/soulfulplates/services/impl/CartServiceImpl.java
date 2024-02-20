7f8034b2e072b75d3e324f52af4006f27cb3d07a
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-02-20 19:06:00 +0530

    feat(Backend): Implemented service method to delete cart and items by cart id

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/CartServiceImpl.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/CartServiceImpl.java
@@ -2,9 +2,11 @@ package com.Group11.soulfulplates.services.impl;
 
 import com.Group11.soulfulplates.models.Cart;
 import com.Group11.soulfulplates.repository.CartRepository;
+import com.Group11.soulfulplates.services.CartItemService;
 import com.Group11.soulfulplates.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -13,9 +15,11 @@ import java.util.Optional;
 public class CartServiceImpl implements CartService {
 
     private final CartRepository cartRepository;
+    private final CartItemService cartItemService;
 
     @Autowired
-    public CartServiceImpl(CartRepository cartRepository) {
+    public CartServiceImpl(CartRepository cartRepository, CartItemService cartItemService) {
+        this.cartItemService = cartItemService;
         this.cartRepository = cartRepository;
     }
 
@@ -58,6 +62,13 @@ public class CartServiceImpl implements CartService {
             newCart.setLastUpdatedDate(LocalDateTime.now());
             return cartRepository.save(newCart);
         }
+    }
+
+    @Override
+    @Transactional
+    public void deleteCartAndItems(Long cartId) {
+        cartItemService.deleteByCartId(cartId); // Delete cart items first
+        cartRepository.deleteById(cartId); // Then delete the cart
     }
 
 }
