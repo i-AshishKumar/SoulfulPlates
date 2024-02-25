22640b73f0133778726ccf1da1276e4a2a27ea24
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-02-25 11:03:43 +0530

    feat(Backend): Implemented the methods in Address Service Interface

---
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/AddressServiceImpl.java
@@ -0,0 +1,42 @@
+package com.Group11.soulfulplates.services.impl;
+
+import com.Group11.soulfulplates.models.Address;
+import com.Group11.soulfulplates.repository.AddressRepository;
+import com.Group11.soulfulplates.services.AddressService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Override
+    public Address createAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    @Override
+    public Address getAddressById(Long id) {
+        return addressRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Address not found."));
+    }
+
+    @Override
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+    @Override
+    public Address updateAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+    @Override
+    public void deleteAddress(Long id) {
+        addressRepository.deleteById(id);
+    }
+}
