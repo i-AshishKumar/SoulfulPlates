b95b25e49b95d5e927a9fea32009039ff9bd566b
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-03-07 06:58:27 +0530

    feat(backend): Store Service and its implementation created to Post Store Details

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/StoreServiceImpl.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/services/impl/StoreServiceImpl.java
@@ -1,51 +1,42 @@
 package com.Group11.soulfulplates.services.impl;
 
 import com.Group11.soulfulplates.models.Store;
+import com.Group11.soulfulplates.repository.StoreRepo;
 import com.Group11.soulfulplates.repository.StoreRepository;
 import com.Group11.soulfulplates.services.StoreService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class StoreServiceImpl implements StoreService {
+
+    private final StoreRepo storeRepository;
+
     @Autowired
-    private StoreRepository storeRepository;
-
-    //    @Autowired
-//    public StoreServiceImpl(StoreRepository storeRepository) {
-//        this.storeRepository = storeRepository;
-//    }
-
-    @Override
-    public Store createStore(Store seller) {
-        return storeRepository.save(seller);
+    public StoreServiceImpl(StoreRepo storeRepo) {
+        this.storeRepository = storeRepo;
     }
 
     @Override
-    public Optional<Store> getStoreById(Long id) {
-        return storeRepository.findById(id);
+    public Store saveStore(Store store) {
+        return storeRepository.save(store);
     }
 
     @Override
-    public List<Store> getAllStores() {
-        return storeRepository.findAll();
+    @Transactional
+    public Store updateStoreByUserId(Long userId, Store storeDetails) throws Exception {
+        Store store = storeRepository.findByUser_Id(userId)
+                .orElseThrow(() -> new Exception("Store not found for this user: " + userId));
+
+        store.setStoreName(storeDetails.getStoreName());
+        store.setStoreEmail(storeDetails.getStoreEmail());
+        store.setContactNumber(storeDetails.getContactNumber());
+        store.setStoreDescription(storeDetails.getStoreDescription());
+
+        return storeRepository.save(store);
     }
 
-    @Override
-    public Store updateStore(Store seller) {
-        return storeRepository.save(seller);
-    }
-
-    @Override
-    public void deleteStore(Long id) {
-        storeRepository.deleteById(id);
-    }
-
-    @Override
-    public boolean existsById(Long id) {
-        return storeRepository.existsById(id);
-    }
 }
