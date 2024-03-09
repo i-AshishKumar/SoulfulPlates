6a9bb8c81caaec38a050d697563a30411aee5f73
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-03-10 01:15:19 +0530

    feat(backend): Endpoint added in Payment Controller to update Payment Status

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/controllers/PaymentController.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/controllers/PaymentController.java
@@ -1,6 +1,8 @@
 package com.Group11.soulfulplates.controllers;
 
 import com.Group11.soulfulplates.payload.request.CreatePaymentRequest;
+import com.Group11.soulfulplates.payload.request.UpdatePaymentStatusRequest;
+import com.Group11.soulfulplates.payload.response.MessageResponse;
 import com.Group11.soulfulplates.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +28,17 @@ public class PaymentController {
             return ResponseEntity.ok().body(Map.of("code", 1, "data", response, "description", "Transaction created."));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(Map.of("code", -1, "description", "An error occurred: " + e.getMessage()));
+        }
+    }
+
+    @PostMapping("/updateStatus")
+    @PreAuthorize("hasRole('ROLE_BUYER')")
+    public ResponseEntity<?> updatePaymentStatus(@RequestBody UpdatePaymentStatusRequest request) {
+        try {
+            paymentService.updatePaymentStatus(request.getPaymentId(), request.getTransactionId(), request.getStatus());
+            return ResponseEntity.ok(new MessageResponse(1, "Payment status updated.", null));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new MessageResponse(0, "Error updating payment status: " + e.getMessage(), null));
         }
     }
 }
