059d9d235f50714db4821034a2a1a3c87632198f
Ashish Kumar Guntipalli <as589490@dal.ca>
2024-03-12 22:24:56 -0300

    test: created test file for PaymentController

---
+++ Backend-Spring-Boot/src/test/java/com/Group11/soulfulplates/controllers/PaymentControllerTest.java
@@ -0,0 +1,35 @@
+package com.Group11.soulfulplates.controllers;
+
+import com.Group11.soulfulplates.payload.request.CreatePaymentRequest;
+import com.Group11.soulfulplates.payload.request.UpdatePaymentStatusRequest;
+import com.Group11.soulfulplates.services.PaymentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class PaymentControllerTest {
+
+    @InjectMocks
+    private PaymentController paymentController;
+
+    @Mock
+    private PaymentService paymentService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+}
+
