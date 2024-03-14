5735ada09fc96d6f66f740a9863e398a57e95e3e
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-03-14 17:44:53 +0530

    feat(backend): Minor Fix Helper Method added to find total of an order

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/utils/CartItemUtils.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/utils/CartItemUtils.java
@@ -1,6 +1,8 @@
 package com.Group11.soulfulplates.utils;
 
 import com.Group11.soulfulplates.models.CartItem;
+import com.Group11.soulfulplates.models.MenuItem;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,5 +12,11 @@ public class CartItemUtils {
         return cartItems.stream() // Convert the list to a stream
                 .map(CartItem::getItemId) // Transform each CartItem to its itemId
                 .collect(Collectors.toList()); // Collect the results into a new list
+    }
+
+    public static Double getTotalForOrderId(List<CartItem> cartItems) {
+        return cartItems.stream() // Convert the list to a stream
+                .map(cartItem -> cartItem.getPrice() * cartItem.getQuantity()) // Calculate the total price for each CartItem
+                .reduce(0.0, Double::sum); // Sum up all the calculated prices
     }
 }
