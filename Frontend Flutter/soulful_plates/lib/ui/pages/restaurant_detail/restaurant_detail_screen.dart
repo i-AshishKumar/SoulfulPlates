5ae4a357410b65219e1563dd029c96645f126aa8
Anjali Rachel Benjamin <an653262@dal.ca>
2024-03-16 00:26:55 -0300

    feat(frontend): Restaurant Details Screen
    - Added bottom sheet for the item cards.

--- Frontend Flutter/soulful_plates/lib/ui/pages/restaurant_detail/restaurant_detail_screen.dart
+++ Frontend Flutter/soulful_plates/lib/ui/pages/restaurant_detail/restaurant_detail_screen.dart
@@ -92,88 +92,130 @@ class RestaurantDetailScreen extends GetView<RestaurantDetailController>
   }
 
   Widget getItemCard() {
-    return Container(
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          8.rHorizontalSizedBox(),
-          Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+        onTap: () {
+          _showItemDetailBottomSheet();
+        },
+        child: Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
+              8.rHorizontalSizedBox(),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    Utils().getFoodTypeIcon('Veg'),
-                    width: 18.rSize(),
-                    height: 18.rSize(),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        Utils().getFoodTypeIcon('Veg'),
+                        width: 18.rSize(),
+                        height: 18.rSize(),
+                      ),
+                      4.rHorizontalSizedBox(),
+                      Text(
+                        "Eggs",
+                        style: AppTextStyles.textStyleBlackTwo14With600,
+                      ),
+                      4.rHorizontalSizedBox(),
+                    ],
                   ),
-                  4.rHorizontalSizedBox(),
+                  2.rVerticalSizedBox(),
                   Text(
-                    "Eggs",
+                    "Punjabi Chhole with Bhature",
+                    maxLines: 2,
+                    style: AppTextStyles.textStyleBlack16With700,
+                  ),
+                  2.rVerticalSizedBox(),
+                  Text(
+                    "\$ 12.45",
                     style: AppTextStyles.textStyleBlackTwo14With600,
                   ),
-                  4.rHorizontalSizedBox(),
+                  4.rVerticalSizedBox(),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1.25,
+                          color: AppColor.black2TextColor.withOpacity(0.75),
+                        ),
+                        borderRadius: BorderRadius.circular(40)),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(
+                          Icons.info_outline,
+                          size: 18,
+                          color: AppColor.black2TextColor,
+                        ),
+                        4.rHorizontalSizedBox(),
+                        Text(
+                          "Item Info",
+                          style: AppTextStyles.textStyleBlackTwo14With600,
+                        ),
+                        4.rHorizontalSizedBox(),
+                      ],
+                    ).paddingAll4(),
+                  ),
+                  4.rVerticalSizedBox(),
                 ],
-              ),
-              2.rVerticalSizedBox(),
-              Text(
-                "Punjabi Chhole with Bhature",
-                maxLines: 2,
-                style: AppTextStyles.textStyleBlack16With700,
-              ),
-              2.rVerticalSizedBox(),
-              Text(
-                "\$ 12.45",
-                style: AppTextStyles.textStyleBlackTwo14With600,
-              ),
-              4.rVerticalSizedBox(),
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      width: 1.25,
-                      color: AppColor.black2TextColor.withOpacity(0.75),
-                    ),
-                    borderRadius: BorderRadius.circular(40)),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Icon(
-                      Icons.info_outline,
-                      size: 18,
-                      color: AppColor.black2TextColor,
-                    ),
-                    4.rHorizontalSizedBox(),
-                    Text(
-                      "Item Info",
-                      style: AppTextStyles.textStyleBlackTwo14With600,
-                    ),
-                    4.rHorizontalSizedBox(),
-                  ],
-                ).paddingAll4(),
-              ),
-              4.rVerticalSizedBox(),
+              ).paddingAllDefault()),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(8.0),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl: "https://static.toiimg.com/photo/62601713.cms",
+                  fit: BoxFit.fill,
+                  color: AppColor.blackTextColor.withOpacity(0.1),
+                  colorBlendMode: BlendMode.color,
+                  width: 100,
+                  height: 100,
+                ),
+              ).paddingUpSide412(),
             ],
-          ).paddingAllDefault()),
-          ClipRRect(
-            borderRadius: const BorderRadius.all(
-              Radius.circular(8.0),
-            ),
-            child: CachedNetworkImage(
-              imageUrl: "https://static.toiimg.com/photo/62601713.cms",
-              fit: BoxFit.fill,
-              color: AppColor.blackTextColor.withOpacity(0.1),
-              colorBlendMode: BlendMode.color,
-              width: 100,
-              height: 100,
-            ),
-          ).paddingUpSide412(),
-        ],
-      ),
-    ).paddingUpSide812();
+          ),
+        ).paddingUpSide812());
   }
+
+  void _showItemDetailBottomSheet() {
+    Get.bottomSheet(
+      Container(
+        height: Get.height / 2, // Cover half of the screen
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Replace the placeholder content with the image
+            Image.network(
+              "https://static.toiimg.com/photo/62601713.cms",
+              width: double.infinity,
+              height: Get.height / 4,
+              fit: BoxFit.cover,
+            ),
+            // Add any additional content you want in the bottom sheet
+            Text(
+              "Item Detail",
+              style: AppTextStyles.textStyleBlack18With700,
+            ),
+            // Add more content here if needed
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(GetMaterialApp(
+    home: RestaurantDetailScreen(),
+  ));
 }
