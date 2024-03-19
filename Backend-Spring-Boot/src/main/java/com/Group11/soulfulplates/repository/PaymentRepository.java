5e29d083d503d28695fc7c79119c56c05da53067
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2024-03-19 17:10:04 +0530

    feat(Backend): Repository method added for Buyer payment history

--- Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/repository/PaymentRepository.java
+++ Backend-Spring-Boot/src/main/java/com/Group11/soulfulplates/repository/PaymentRepository.java
@@ -4,16 +4,14 @@ import com.Group11.soulfulplates.models.Payment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<Payment> findByOrderOrderId(Long orderId);
     Optional<Payment> findFirstByOrderOrderIdOrderByPaymentIdDesc(Long orderId);
-
-    Page<Payment> findByOrderUserUserId(Long userId, Pageable pageable);
-    Page<Payment> findByOrderUserUserIdAndStatus(Long userId, String status, Pageable pageable);
-
-    Page<Payment> findByStoreStoreId(Long storeId, Pageable pageable);
-    Page<Payment> findByStoreStoreIdAndStatus(Long storeId, String status, Pageable pageable);
+    Page<Payment> findByTransactionUserIdAndStatusOrderByCreatedAtDesc(Long userId, String status, Pageable pageable);
 }
